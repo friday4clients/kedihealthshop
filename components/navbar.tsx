834bb00cb7b0655b5ce45de58a0ea08cd8114094
Friday Joshua <fridayjoshua952@gmail.com>
2025-04-22 08:27:32 +0100

    feat: Update mobile drawer menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
-import { Container, Box, HStack, Image, IconButton } from '@chakra-ui/react';
+import { Container, Box, HStack, Image, IconButton, Stack, Drawer, Portal, CloseButton, Heading } from '@chakra-ui/react';
 import { LuAlignRight } from 'react-icons/lu';
 import { FaFacebook } from 'react-icons/fa';
+import { getCategories } from '@/lib/utils';
 
 
-const Navbar = () => {
+
+const Navbar = async () => {
+    const categories: Awaited<ReturnType<typeof getCategories>> = await getCategories();
+
     return (
         <Container display={{ base: "block", md: "none" }} maxW={"6xl"} as="nav" bg="white" shadow="0 0 10px #ddd" >
             <HStack px="4" h="14" justifyContent={{ base: "space-between" }}>
@@ -26,9 +30,48 @@ const Navbar = () => {
                         Contact
                     </Link>
                 </HStack> */}
-                <IconButton display={{ md: "none" }}>
-                    <LuAlignRight />
-                </IconButton>
+
+
+                <Drawer.Root>
+                    <Drawer.Trigger asChild>
+                        <IconButton display={{ md: "none" }}>
+                            <LuAlignRight />
+                        </IconButton>
+                    </Drawer.Trigger>
+                    <Portal>
+                        <Drawer.Backdrop />
+                        <Drawer.Positioner>
+                            <Drawer.Content bg="white">
+                                <Drawer.Header>
+                                    <Drawer.Title>Menu</Drawer.Title>
+                                </Drawer.Header>
+                                <Drawer.Body>
+                                    <Stack gap="4">
+                                        <Link href="/">Home</Link>
+                                        <Link href="/category">Category</Link>
+                                        <Link href="/about">About</Link>
+                                        <Link href="/services">Services</Link>
+                                        <Link href="/contact">Contact</Link>
+                                    </Stack>
+
+                                    <Stack mt="6" gap="4">
+                                        <Heading fontSize="md" fontWeight="sm">Categories</Heading>
+                                        {categories.map((category, index) => (
+                                            <Link key={index} href={`/${category.replaceAll(" ","_")}`}>
+                                                {category}
+                                            </Link>
+                                        ))}
+                                    </Stack>
+                                </Drawer.Body>
+                                <Drawer.Footer>
+                                </Drawer.Footer>
+                                <Drawer.CloseTrigger asChild>
+                                    <CloseButton color="black" size="sm" />
+                                </Drawer.CloseTrigger>
+                            </Drawer.Content>
+                        </Drawer.Positioner>
+                    </Portal>
+                </Drawer.Root>
             </HStack>
         </Container>
     );
