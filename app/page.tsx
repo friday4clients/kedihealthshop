cd8f97acc4cdf3f6db4b6a8493c7463d5c55f7f2
Friday Joshua <fridayjoshua952@gmail.com>
2025-04-24 11:07:15 +0100

    feat: Update home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Heading, Image, Text, Icon, Box, Grid, GridItem, HStack, Highlight, Stat, Separator, Stack } from "@chakra-ui/react";
+import { Button, Container, Heading, Image, Text, Icon, Box, Grid, GridItem, HStack, Highlight, Stat, Separator, Stack, RatingGroup, Accordion, Input } from "@chakra-ui/react";
 import Link from "next/link";
 import { getCategories, getStore } from "@/lib/utils";
 import { getProducts } from "@/lib/utils";
@@ -6,6 +6,7 @@ import { LuArrowRight } from "react-icons/lu";
 import HeroCarousel from "@/components/hero_carousel";
 import { BiBadgeCheck, BiSolidBadge, BiSupport } from "react-icons/bi";
 import { FaMoneyBillWave } from "react-icons/fa";
+import Testimonials from "@/components/testimonial";
 
 
 
@@ -248,7 +249,7 @@ export default async function Home() {
       </Box>
 
       {/* Features Section */}
-      <Box py="32">
+      <Box pt={{ base: "0", md: "12" }} pb={{ base: "24", md: "52" }}>
         <Container maxW="6xl">
           <Heading
             as="h2"
@@ -257,13 +258,15 @@ export default async function Home() {
             fontFamily="merriweather"
             textAlign="center"
             mb="8"
+            w={{ base: "50%", md: "full" }}
+            mx="auto"
           >
             Why Choose Us
           </Heading>
           <Grid
             templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
             gap="6"
-            className="*:nth-[odd]:!translate-y-12"
+            className="md:*:nth-[odd]:!translate-y-12"
           >
             {[
               {
@@ -287,7 +290,7 @@ export default async function Home() {
             ].map((feature, index) => (
               <Stack
                 key={index}
-                p="6"
+                p="10"
                 gap="6"
                 borderWidth="sm"
                 borderColor="border"
@@ -318,112 +321,118 @@ export default async function Home() {
       </Box>
 
       {/* Services Section */}
-      <Box py="12" bg="white">
+      <Box py={{ base: "20", md: "40" }} bg="white">
         <Container maxW="6xl">
           <Heading
             as="h2"
-            size="3xl"
+            size={{ base: "3xl", md: "5xl" }}
             fontWeight="bold"
             fontFamily="merriweather"
             textAlign="center"
-            mb="8"
+            mb="16"
           >
             Our Services
           </Heading>
-          <Grid
-            templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
-            gap="6"
+          <Stack gap={{ base: "12", md: "20" }}
+          // className="*:nth-[odd]:!-translate-y-12"
           >
             {[
               {
                 title: "Home Delivery",
                 description:
                   "Enjoy the convenience of having your favorite Kedi products delivered right to your doorstep.",
-                icon: "/delivery_icon.svg",
+                icon: "/home_delivery_man.jpg",
               },
               {
                 title: "In-Store Purchase",
                 description:
                   "Visit our store to explore a wide range of products and get personalized assistance.",
-                icon: "/instore_icon.svg",
+                icon: "/instore_purchase.jpg",
               },
               {
                 title: "Health Consultations",
                 description:
                   "Get expert advice and consultations to choose the best products for your health needs.",
-                icon: "/consultation_icon.svg",
+                icon: "/health_consultation.jpg",
               },
             ].map((service, index) => (
-              <Stack
-                key={index}
-                p="6"
-                borderWidth="sm"
-                borderColor="border"
-                borderRadius="lg"
-                bg="white"
-                shadow="xs"
-                textAlign="center"
-              >
-                <Image
-                  src={service.icon}
-                  alt={service.title}
-                  boxSize="50px"
-                  mx="auto"
-                  mb="4"
-                />
-                <Heading as="h3" size="lg" fontWeight="semibold" mb="2">
-                  {service.title}
-                </Heading>
-                <Text color="gray.600">{service.description}</Text>
-              </Stack>
+              <Grid
+                templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+                gap={{ base: "3", md: "12" }}
+                key={index}>
+                <GridItem
+                  order={{ base: "0", md: index === 1 ? "1" : "0" }}
+                  bgImg={`url(${service.icon})`}
+                  bgSize={"cover"}
+                  bgPos="center"
+                  h="300px"
+                  rounded="xl"
+                ></GridItem>
+                <GridItem>
+                  <Stack
+                    p="6"
+                    gap={{ base: "3", md: "6" }}
+                  >
+                    <Heading
+                      color="accent"
+                      as="h3"
+                      size={{ base: "2xl", md: "3xl" }}
+                      fontWeight="bold" >
+                      {service.title}
+                    </Heading>
+                    <Text
+                      textStyle={{ base: "2xl", md: "5xl" }}
+                      fontWeight={"lighter"}>{service.description}</Text>
+                  </Stack>
+                </GridItem>
+              </Grid>
             ))}
-          </Grid>
+          </Stack>
         </Container>
       </Box>
 
       {/* Call to Action Section */}
-      <Box py="12" bg="blue.100">
+      <Box
+        py="12"
+        bgImg={"linear-gradient(to bottom, {colors.accent}, {colors.blue.100})"}
+        bgSize={"cover"}>
         <Container maxW="6xl">
-          <Grid
-            templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
-            alignItems="center"
-            gap="6"
-          >
-            <Box>
-              <Heading
-                as="h2"
-                size="3xl"
-                fontWeight="bold"
-                fontFamily="merriweather"
-                mb="4"
-              >
-                Ready to Transform Your Health?
-              </Heading>
-              <Text color="gray.700" mb="6">
-                Discover the benefits of Kedi products today. Join thousands of satisfied customers who trust Kedicares for their wellness journey.
-              </Text>
-              <Button
-                variant="solid"
-                color="white"
-                bg="accent"
-                _hover={{ bg: "blue.muted" }}
-                size="lg"
-              >
-                Get Started
-              </Button>
-            </Box>
-            <Image
-              src="/call_to_action_image.jpg"
-              alt="Call to Action"
-              borderRadius="lg"
-              shadow="md"
-            />
-          </Grid>
+          <Stack gap="4" textAlign={"center"} alignItems={"center"}>
+            <Heading
+              as="h2"
+              size="3xl"
+              fontWeight="bold"
+              fontFamily="merriweather"
+              color="white"
+              mb="4"
+            >
+              Ready to Transform Your Health?
+            </Heading>
+            <Text
+              w={{ base: "90%", md: "70%" }}
+              color="blue.50"
+              mb="6">
+              Discover the benefits of Kedi products today. Join thousands of satisfied customers who trust Kedicares for their wellness journey.
+            </Text>
+            <Button
+              variant="solid"
+              color="white"
+              bg="accent"
+              w="fit"
+              _hover={{ bg: "blue.muted" }}
+              size="lg"
+            >
+              Get Started
+            </Button>
+          </Stack>
         </Container>
       </Box>
 
+      {/* Testimonials Section */}
+      <Testimonials />
+
       {/* FAQ Section */}
-      <Box py="12" bg="white">
+      <Box py={{ base: "20", md: "40" }} bg="white">
         <Container maxW="6xl">
           <Heading
             as="h2"
@@ -436,96 +445,66 @@ export default async function Home() {
             Frequently Asked Questions
           </Heading>
           <Stack gap="6">
-            {[
-              {
-                question: "What are Kedi products?",
-                answer:
-                  "Kedi products are high-quality health and wellness products designed to improve your overall well-being.",
-              },
-              {
-                question: "How can I place an order?",
-                answer:
-                  "You can place an order directly on our website or contact our support team for assistance.",
-              },
-              {
-                question: "Do you offer international shipping?",
-                answer:
-                  "Yes, we offer international shipping to many countries. Please check our shipping policy for more details.",
-              },
-            ].map((faq, index) => (
-              <Box key={index}>
-                <Heading as="h3" size="md" mb="2">
-                  {faq.question}
-                </Heading>
-                <Text color="gray.600">{faq.answer}</Text>
-              </Box>
-            ))}
-          </Stack>
-        </Container>
-      </Box>
+            <Accordion.Root
+              w="full"
+              variant={"subtle"}
+              size="lg"
+              gap="4"
+              collapsible>
+              {[
+                {
+                  question: "When did Kedi commence business in Nigeria?",
+                  answer:
+                    "Kedi Healthcare received CAC registration and NAFDAC approval in 2006 and began operations the same year.",
+                },
+                {
+                  question: "Is Kedi a Nigerian company?",
+                  answer:
+                    "Kedi Healthcare Industries Nigeria Ltd is a wholly‑owned subsidiary of Kedi Healthcare Industries Co. (Hong Kong). Raw materials are imported from China; packaging is done locally by Fidson Healthcare PLC.",
+                },
+                {
+                  question: "How can I place an order?",
+                  answer:
+                    "You can place an order by contacting our office through email, Whatsappp, or phone call, find more details in the contact us page",
+                },
+                {
+                  question: "What kind of products does Kedi produce?",
+                  answer:
+                    "Kedi is a network‑marketing firm that offers nutritional supplements, herbal beverages, personal‑care items, and health‑diagnostic/therapy equipment.",
+                },
+                {
+                  question: "What type of diseases can Kedi products help with?",
+                  answer:
+                    "As nutritional supplements, Kedi products may support management of diabetes, stroke, menopausal syndrome, infertility, hypertension, impotence, asthma, cancer, fibroids, and more; they are not positioned as pharmaceutical cures.",
+                },
+                {
+                  question: "How do I store KEDI products?",
+                  answer: "The storage condition for KEDI products is stated on the product label and leaflet, it is advised to have a clean, conducive and safe storage facility and follow the storage conditions stated on the product label and leaflet to avoid any form of damage.Keep the product tightly closed after opening and store same in a dry and cool place or keep delicate product in the fridge after opening, when transporting large quantity of products, ensure that the means of transportation has a proper storage facility, avoid squeezing the pack or bottles when packing the products, avoid placing the products close to the engine of the vehicle to prevent heat damage, ensure that the products does not spend too many days in transit."
+                }
 
-      {/* Testimonials Section */}
-      <Box bg="gray.50" py="12">
-        <Container maxW="6xl">
-          <Heading
-            as="h2"
-            size="3xl"
-            fontWeight="bold"
-            fontFamily="merriweather"
-            textAlign="center"
-            mb="8"
-          >
-            What Our Customers Say
-          </Heading>
-          <Grid
-            templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
-            gap="6"
-          >
-            {[
-              {
-                name: "Jane Doe",
-                feedback:
-                  "Kedi products have changed my life! The quality and affordability are unmatched.",
-                image: "/customer1.jpg",
-              },
-              {
-                name: "John Smith",
-                feedback:
-                  "I love shopping here. The customer service is excellent, and the products are top-notch.",
-                image: "/customer2.jpg",
-              },
-              {
-                name: "Emily Johnson",
-                feedback:
-                  "Highly recommend! The variety of products and the prices are amazing.",
-                image: "/customer3.jpg",
-              },
-            ].map((testimonial, index) => (
-              <Stack
-                key={index}
-                p="6"
-                borderWidth="sm"
-                borderColor="border"
-                borderRadius="lg"
-                bg="white"
-                shadow="xs"
-                textAlign="center"
-              >
-                <Image
-                  src={testimonial.image}
-                  alt={testimonial.name}
-                  boxSize="100px"
-                  borderRadius="full"
-                  mx="auto"
-                  mb="4"
-                />
-                <Text fontWeight="semibold" mb="2">
-                  {testimonial.name}
-                </Text>
-                <Text color="gray.600">{testimonial.feedback}</Text>
-              </Stack>
-            ))}
-          </Grid>
+              ].map((faq, index) => (
+                <Accordion.Item
+                  _open={{ bg: "gray.100" }}
+                  rounded="xl"
+                  py="2"
+                  key={index}
+                  value={faq.answer}>
+                  <Accordion.ItemTrigger
+                    cursor={"pointer"}>
+                    <Heading _hover={{ color: "accent" }} flex="1" as="h3" size="md">
+                      {faq.question}
+                    </Heading>
+                    <Accordion.ItemIndicator />
+                  </Accordion.ItemTrigger>
+                  <Accordion.ItemContent>
+                    <Accordion.ItemBody>
+                      <Text>{faq.answer}</Text>
+                    </Accordion.ItemBody>
+                  </Accordion.ItemContent>
+                </Accordion.Item>
+              ))}
+            </Accordion.Root>
+          </Stack>
         </Container>
       </Box>
 
@@ -552,23 +531,21 @@ export default async function Home() {
             gap="4"
           >
             <Box flex="1">
-              <input
+              <Input
                 type="email"
                 placeholder="Enter your email"
-                style={{
-                  width: "100%",
-                  padding: "12px",
-                  borderRadius: "8px",
-                  border: "1px solid #CBD5E0",
-                  outline: "none",
-                }}
+                width="100%"
+                padding="12px"
+                borderRadius="8px"
+                border="1px solid #CBD5E0"
+                outline="none"
                 required
               />
             </Box>
             <Button
               type="submit"
-              colorScheme="blue"
               size="lg"
+              color="blue.50"
               px="8"
               bg="accent"
               _hover={{ bg: "blue.muted" }}
@@ -578,29 +555,6 @@ export default async function Home() {
           </HStack>
         </Container>
       </Box>
-
-
     </>
   );
 }
-
-const ImmuneWellnessIcon = () => {
-  return (
-    <svg
-      fill="accent"
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 52 52"
-    >
-      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-      <g id="SVGRepo_iconCarrier">
-        <path d="M27.42,10.57A1.66,1.66,0,0,0,26.17,10h0a1.66,1.66,0,0,0-1.25.56c-11.76,13.39-5,24,0,29.07a1.66,1.66,0,0,0,1.19.49h0a1.77,1.77,0,0,0,1.24-.54C32.3,34.48,38.83,23.93,27.42,10.57Z"></path>
-        <path d="M15.66,28.3C13,26.79,9.37,25.92,4.56,26.24a1.27,1.27,0,0,0-1,.53h0a1.34,1.34,0,0,0-.24,1.08c3,14,13,14.75,18.77,13.94a1.35,1.35,0,0,0,.64-.3A23.14,23.14,0,0,1,15.66,28.3Z"></path>
-        <path d="M36.34,28.3c2.69-1.51,6.29-2.38,11.1-2.06a1.27,1.27,0,0,1,1,.53h0a1.34,1.34,0,0,1,.24,1.08c-3,14-13,14.75-18.77,13.94a1.35,1.35,0,0,1-.64-.3A23.14,23.14,0,0,0,36.34,28.3Z"></path>
-        <path d="M15.38,25c.32-4.56,1.2-6.62,1.61-7.35a22.71,22.71,0,0,0-6.43-3.46,1.28,1.28,0,0,0-1.08.09h0a1.34,1.34,0,0,0-.64.88,21.65,21.65,0,0,0-.25,8.41A22.93,22.93,0,0,1,15.38,25Z"></path>
-        <path d="M43.31,15.13a1.29,1.29,0,0,0-.64-.88h0a1.28,1.28,0,0,0-1.08-.09,22.79,22.79,0,0,0-6.42,3.46c.4.73,1.29,2.79,1.6,7.35a22.93,22.93,0,0,1,6.8-1.43A21.27,21.27,0,0,0,43.31,15.13Z"></path>
-      </g>
-    </svg>
-  );
-};
