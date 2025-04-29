74468f6ea44295078f7c1f6b4650befada79ecba
Friday Joshua <fridayjoshua952@gmail.com>
2025-04-29 21:49:21 +0100

    feat: Add about us route

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,4 +1,4 @@
-import { Container, HStack, Icon } from '@chakra-ui/react';
+import { Box, Container, Grid, GridItem, Heading, HStack, Icon, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
@@ -10,9 +10,109 @@ const socialLinks = [
 
 const AboutUsPage = () => {
     return (
-        <Container>
+        <>
+            <Box bg="accent" lineHeight={"2"}>
+                <Container maxW="6xl">
+                    <Stack py="12" alignItems="center" gap="6">
+                        <Heading
+                            size={{ base: "3xl", md: "5xl" }}
+                            color="white"
+                            fontFamily={"merriweather"}
+                        >About {process.env.NEXT_PUBLIC_SITE_NAME}</Heading>
+                        <Text textAlign="center" color="blue.200">
+                            {process.env.NEXT_PUBLIC_SITE_NAME} is proud to be a trusted partner and distributor of Kedi Healthcare Industries Hong-Kong Limited, located in Hong-Kong China, bringing health and wellness solutions to communities far and wide. Our mission is to empower individuals and families by providing access to high-quality, scientifically-backed healthcare products. Join us on this journey to make a positive impact on lives through innovation and dedication.
+                        </Text>
+                    </Stack>
+                </Container>
+            </Box>
 
-        </Container>
+            <Box bg="white" py="12" lineHeight={"2"}>
+                <Container maxW="6xl">
+                    <Grid gap={{ base: "4", md: "0" }} templateColumns={{ base: "1fr", md: "1fr 1fr 1fr" }}>
+                        <GridItem
+                            display="flex"
+                            alignItems="center"
+                            h="full"
+                            justifyContent={{ md: "center" }}>
+                            <Heading w="1/2" size="4xl" color="accent">
+                                About Kedi
+                            </Heading>
+                        </GridItem>
+                        <GridItem colSpan={2}>
+                            <Text>
+                                The word KEDI is a Chinese language meaning "Science is unlimited". Kedi healthcare is a giant Multi-level marketing company of our time, entered Nigeria in 2005, utilized the period to accomplish all due process in line with all rules and regulation in the country and commenced operation specifically on 10th June, 2006 very much in contrast with other Multi-level companies
+                            </Text>
+                            <Text mt="4">
+                                As part of her overall strategy to build a formidable force in the field of life sciences, KEDI has invested tremendously in research and development by partnering with Research and Development Institutes and Manufacturing Companies across the world to continuously connote a culture that combines an endorsement of globally acceptable standards in healthcare industry practices with a genuine concern for the availability of potent but affordable medical products and equipment to meet the needs of her customers and form a powerful synergy based on a set of values in Africa and beyond. Our product lines have covered Herbal Medicine/TCM, Food Supplements, Personal Care and Medical Device.
+                            </Text>
+                        </GridItem>
+                    </Grid>
+                </Container>
+            </Box>
+
+            <Box bg="white" py="12" lineHeight={"2"}>
+                <Container maxW="6xl">
+                    <Grid gap={{ base: "4", md: "0" }} templateColumns={{ base: "1fr", md: "1fr 1fr 1fr" }}>
+                        <GridItem colSpan={2}>
+                            <Text>
+                                Making people&apos;s lives better by unleashing the power of healthcare, KEDI Healthcare aims to setup Traditional Chinese Medicine Pharmaceutical Factories, Traditional Chinese Medicine Hospitals and Chinese Herbal Medicine Research Institutes worldwide                            </Text>
+                        </GridItem>
+                        <GridItem
+                            order={{ base: "-1", md: "1" }}
+                            display="flex"
+                            alignItems="center"
+                            h="full"
+                            justifyContent={{ md: "center" }}>
+                            <Heading w="1/2" size="4xl" color="accent">
+                                Kedi&apos;s Vision
+                            </Heading>
+                        </GridItem>
+                    </Grid>
+                </Container>
+            </Box>
+
+            <Box bg="white" py="12" lineHeight={"2"}>
+                <Container maxW="6xl">
+                    <Grid gap={{ base: "4", md: "0" }} templateColumns={{ base: "1fr", md: "1fr 1fr 1fr" }}>
+                        <GridItem
+                            display="flex"
+                            alignItems="center"
+                            h="full"
+                            justifyContent={{ md: "center" }}>
+                            <Heading w="1/2" size="4xl" color="accent">
+                                Kedi&apos;s Mission
+                            </Heading>
+                        </GridItem>
+                        <GridItem colSpan={2}>
+                            <Text>
+                                KEDI Healthcare commits itself to promoting Traditional Chinese Medicine (TCM) culture worldwide, introducing the quintessence of Chinese Medicine and contributing to the wellbeing of mankind.                            </Text>
+                        </GridItem>
+                    </Grid>
+                </Container>
+            </Box>
+
+            <Box bg="white" py="12" lineHeight={"2"}>
+                <Container maxW="6xl">
+                    <Grid templateColumns={{ base: "1fr", md: "1fr 1fr 1fr" }}>
+                        <GridItem colSpan={2}>
+                            <Text>
+                                To provide our customers with healthcare solutions to suit their needs and to provide these solutions at an affordable price.
+                            </Text>
+                        </GridItem>
+                        <GridItem
+                            order={{ base: "-1", md: "1" }}
+                            display="flex"
+                            alignItems="center"
+                            h="full"
+                            justifyContent={{ md: "center" }}>
+                            <Heading w="1/2" size="4xl" color="accent">
+                                Kedi&apos;s Goal
+                            </Heading>
+                        </GridItem>
+                    </Grid>
+                </Container>
+            </Box>
+        </>
     );
 };
 
