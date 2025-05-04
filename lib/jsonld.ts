2557fea76960d78ccc23ce45882d183de3ecb821
friday4code <fridayjoshua.dev@gmail.com>
2025-05-04 16:02:02 +0100

    fix: Resolve JSONLD

--- lib/jsonld.ts
+++ lib/jsonld.ts
@@ -9,8 +9,8 @@ export const getCategoryJSONLD = async (category: CategoryType, products: Produc
         "@context": "https://schema.org",
         "@type": "WebPage",
         "name": toTitleCase(category.category),
-        "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${category.category.toLowerCase()?.replaceAll(" ", "_")}`,
-        "description": [category.description.toLowerCase()],
+        "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${category.category?.replaceAll(" ", "_")}`,
+        "description": [category.description],
         "image": "/logo.webp",
         "mainEntity": {
             "@type": "ItemList",
@@ -18,7 +18,7 @@ export const getCategoryJSONLD = async (category: CategoryType, products: Produc
                 return {
                     "@type": "Product",
                     "name": (product?.title as string),
-                    "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${encodeURIComponent((product?.category as string)?.replaceAll(" ", "_"))}/${encodeURIComponent((product?.title as string)?.replaceAll(" ", "_"))}?product_id=${encodeURIComponent(product?.product_id as string)}`,
+                    "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${encodeURIComponent((product?.category as string))}?product_id=${product?.product_id}`,
                     "image": product?.img_url as string,
                     "description": (product?.description as string),
                     "brand": {
@@ -44,23 +44,23 @@ export const getProductJSONLD = async (product: ProductType) => {
         "@context": "https://schema.org",
         "@type": "Product",
         "name": (product?.title as string),
-        "url": `${process.env.APP_URL}/${encodeURIComponent((product?.category as string)?.replaceAll(" ", "-"))}/${encodeURIComponent((product?.title as string)?.replaceAll(" ", "-"))}?productId=${encodeURIComponent(product?.product_id as string)}`,
+        "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${encodeURIComponent((product?.category as string)?.replaceAll(" ", "_"))}/${encodeURIComponent((product?.title as string))}?product_id=${product?.product_id}`,
         "image": product?.img_url,
         "description": (product?.description as string),
         "brand": {
             "@type": "Brand",
-            "name": "Forever Living Products"
+            "name": process.env.NEXT_PUBLIC_SITE_NAME
         },
         "offers": {
             "@type": "Offer",
-            "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${encodeURIComponent((product?.category as string)?.replaceAll(" ", "_"))}/${encodeURIComponent((product?.title as string)?.replaceAll(" ", "_"))}?product_id=${encodeURIComponent(product?.product_id as string)}`,
+            "url": `${process.env.NEXT_PUBLIC_HOSTNAME}/${encodeURIComponent((product?.category as string)?.replaceAll(" ", "_"))}/${encodeURIComponent((product?.title as string))}?product_id=${product?.product_id}`,
             "priceCurrency": "NGN",
             "price": product?.price as string,
             "itemCondition": "https://schema.org/NewCondition",
             "availability": "https://schema.org/InStock",
             "seller": {
                 "@type": "Organization",
-                "name": process.env.NEXT_PUBLIC_HOSTNAME
+                "name": process.env.NEXT_PUBLIC_SITE_NAME
             }
         }
     }
