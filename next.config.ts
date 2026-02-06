779b1112e9d912c1cc4c0b40e76947f7785cd80c
friday4code <fridayjoshua.dev@gmail.com>
2026-02-06 10:36:44 +0100

    feat: Enable new JSX transform, include development types, and re-enable ESLint during builds.

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-  eslint:{
-    ignoreDuringBuilds:true
-  }
 };
 
 export default nextConfig;
